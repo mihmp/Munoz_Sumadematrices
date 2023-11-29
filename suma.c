56776dcee79a8ce88e941174a788575ad5f7a1f0
Pablo <pablo.munoz.proano@udla.edu.ec>
2023-11-29 15:41:22 -0500

    Se agrega una condicion por si se ingresa un numero menor a 0, se prosigue con mas lineas de codigo

--- suma.c
+++ suma.c
@@ -4,15 +4,21 @@
 
 int main(int argc, char const *argv[])
 {
-    srand(time(NULL)); //Ingreso semilla de numeros aleatorios//
+    srand(time(NULL)); // Ingreso semilla de numeros aleatorios//
 
-    int filas, columnas; //Defino variables//
+    int filas, columnas; // Defino variables//
 
     printf("Ingrese el numero de filas: ");
     scanf("%d", &filas);
 
     printf("Ingrese el numero de columnas: ");
     scanf("%d", &columnas);
+
+    if (filas <= 0 || columnas <= 0)
+    {
+        printf("No es valida las dimensiones, debe ser un numero mayor a 0.\n");
+        return 1; //Se cierra por el error//
+    }
 
     return 0;
 }
