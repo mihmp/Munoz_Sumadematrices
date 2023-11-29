ee1ae3ea74ca85413f00ebc9c471eb18be7d86f3
Pablo <pablo.munoz.proano@udla.edu.ec>
2023-11-29 15:33:05 -0500

    Se agregan nuevas lineas de codigo, se pide que se ingrese el numero de filas y columnas

--- suma.c
+++ suma.c
@@ -1,5 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define dim_max 20
+int main(int argc, char const *argv[])
+{
+    srand(time(NULL)); //Ingreso semilla de numeros aleatorios//
+
+    int filas, columnas; //Defino variables//
+
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &filas);
+
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &columnas);
+
+    return 0;
+}
