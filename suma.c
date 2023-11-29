168709faf082f97dccd652cc408dcc186a6366e3
Pablo <pablo.munoz.proano@udla.edu.ec>
2023-11-29 15:50:42 -0500

    Se agregan nuevas lineas de codigo, lleno y uestro la matriz en pantalla

--- suma.c
+++ suma.c
@@ -17,7 +17,23 @@ int main(int argc, char const *argv[])
     if (filas <= 0 || columnas <= 0)
     {
         printf("No es valida las dimensiones, debe ser un numero mayor a 0.\n");
-        return 1; //Se cierra por el error//
+        return 1; // Se cierra por el error//
+    }
+    // Declaro respuesta y matrices//
+    int matrizA[filas][columnas];
+    int matrizB[filas][columnas];
+    int answer[filas][columnas];
+
+    // Lleno matriz A y la muestro en pantalla//
+    printf("Matriz A:\n");
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; i < columnas; i++)
+        {
+            matrizA[i][j] = rand() % 101; // Numeros aleatorios del 0 al 100//
+            printf("%d\t", matrizA[i][j]);
+        }
+        printf("\n"); //Salto de linea//
     }
 
     return 0;
